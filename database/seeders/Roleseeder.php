33bf7de71809dd3519a36a39de4dc8be598644fc
-m <-mkhalidhissoune962@gmail.com>
2024-03-03 11:34:08 +0100

    first commit

---
+++ database/seeders/Roleseeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class Roleseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'client']);
+        Permission::create(['name' => 'client']);
+    }
+}
